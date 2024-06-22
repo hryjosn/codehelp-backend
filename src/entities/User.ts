df58e793f721c3e5548050c6d34e90713e345d3b
KevinLai02 <jihn.smith629@gmail.com>
2024-06-22 15:49:48 +0800

    setting User entities

---
+++ src/entities/User.ts
@@ -0,0 +1,43 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm"
+
+@Entity("user")
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string
+
+  @Column("varchar", { length: 100 })
+  password?: string
+
+  @Column("varchar", { length: 30 })
+  user_name: string
+
+  @Column("varchar", { length: 254, default: "" })
+  email: string
+
+  @Column("varchar", { length: 150 })
+  avatar: string
+
+  @Column({ default: false })
+  verify: boolean
+
+  @Column({ default: false })
+  emailOTP: boolean
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  toJSON() {
+    delete this.password
+    return this
+  }
+}
