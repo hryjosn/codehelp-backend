9b64869e75b97761403a9e11442c2996406f43f2
IR0ng <zhouirong0420@gmail.com>
2024-08-13 23:44:10 +0800

    Modified Route

--- src/index.ts
+++ src/index.ts
@@ -42,8 +42,8 @@ const init = async () => {
     })
   })
   const port = process.env.PORT
-  serverForSocket.listen(Number(port) || 3000, () => {
-    console.log(`App running on port ${Number(port) || 3000}.`)
+  serverForSocket.listen(Number(port) || 3001, () => {
+    console.log(`App running on port ${Number(port) || 3001}.`)
   })
 }
 export default init()
