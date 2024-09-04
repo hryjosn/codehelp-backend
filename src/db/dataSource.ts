2e05c76fe6d84c94736af83f0f67a2f365c7d320
IR0ng <zhouirong0420@gmail.com>
2024-09-04 20:32:24 +0800

    Fix cannot load entities error

--- src/db/dataSource.ts
+++ src/db/dataSource.ts
@@ -1,12 +1,13 @@
 import "dotenv/config"
+import path from "path"
 import { DataSource } from "typeorm"
 
 const dataSource = new DataSource({
   type: "postgres",
   url: process.env.DB_URL,
   synchronize: false,
-  entities: [__dirname + "/entities/*.ts"],
-  migrations: [__dirname + "/migrations/*.ts"],
+  entities: [path.join(__dirname, "/entities/*{.ts,.js}")],
+  migrations: [path.join(__dirname, "/migrations/*{.ts,.js}")],
   extra: {
     max: 1,
   },
