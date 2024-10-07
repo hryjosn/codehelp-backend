8f30aaf3f7677496f19c814b035927bd4039bb63
hryjosn <jo74705@gmail.com>
2024-10-07 21:39:44 +0800

    Refactor member.controller.ts to improve code organization and error handling

--- src/Member/member.controller.ts
+++ src/Member/member.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
-import { save, login as memberLogin } from "./member.feature"
 import { RESPONSE_CODE } from "~/types"
 import FeatureError from "~/utils/FeatureError"
+import { login as memberLogin, save } from "./member.feature"
 
 interface IApi {
   (req: Request, res: Response): void
@@ -10,16 +10,24 @@ interface IApi {
 export const signUp: IApi = async (req, res) => {
   try {
     const files = req.files as { [fieldname: string]: Express.Multer.File[] }
-    const { avatar } = files
+    if (files) {
+      const { avatar } = files
 
-    const { newMember, token } = await save({ ...req.body, avatar })
+      const { newMember, token } = await save({ ...req.body, avatar })
 
-    return res.status(200).send({
-      newMember,
-      status: "ok",
-      message: `${newMember.userName} sign up successful!`,
-      token,
-    })
+      return res.status(200).send({
+        newMember,
+        status: "ok",
+        message: `${newMember.userName} sign up successful!`,
+        token,
+      })
+    } else {
+      res.status(400).send({
+        code: RESPONSE_CODE.VALIDATE_ERROR,
+        message: "Please upload an avatar",
+      })
+      // TODO need to validate the parameters
+    }
   } catch (error) {
     if (error instanceof FeatureError) {
       res.status(error.serverStatus).send({
