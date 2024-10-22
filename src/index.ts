1daffd32e49cd070e191529e846887902ca480b7
andyWeb2022 <andy072892live@gmail.com>
2024-10-22 16:48:12 +0800

    Delete annotation

--- src/index.ts
+++ src/index.ts
@@ -41,29 +41,23 @@ const init = async () => {
     console.log("connect")
 
     socket.on("join", (room) => {
-      console.log("join")
       socket.join(room)
-      socket.to(room).emit("ready", "準備通話")
+      socket.to(room).emit("ready", "Getting ready to start conference")
     })
 
-    // 轉傳 Offer
     socket.on("offer", (room, description) => {
       socket.to(room).emit("offer", description)
     })
 
-    // 轉傳 Answer
     socket.on("answer", (room, description) => {
       socket.to(room).emit("answer", description)
     })
 
-    // 交換 ice candidate
     socket.on("ice_candidate", (room, data) => {
       socket.to(room).emit("ice_candidate", data)
     })
 
-    // 關閉通話
     socket.on("hangup", (room) => {
-      console.log("hangup")
       socket.leave(room)
     })
 
